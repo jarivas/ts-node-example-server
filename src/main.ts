fadd6bde67f0592d8a33a879c5139dac05ffc5a2
Jose Rivas <me@jarivas.work>
2023-08-27 15:42:15 +0000

    Adding CORS

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as restify from 'restify'
+import corsMiddleware from 'restify-cors-middleware2'
 import City from './city.js'
 import Cache from './cache.js'
 import respond from './weather-respond.js'
@@ -8,7 +9,15 @@ const server = restify.createServer()
 
 Cache.on('del', (key: string) => Cache.set(key, new City(key)))
 
+
+const cors = corsMiddleware({
+  preflightMaxAge: 5, //Optional
+  origins: ['*'],
+})
+
 server.use(restify.plugins.queryParser())
+server.pre(cors.preflight)
+server.use(cors.actual)
 server.on('close', () => Cache.close())
 
 server.get('/weather/:name', respond)
